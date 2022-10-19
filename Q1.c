8619c134e161f1bea5218108a42f0f71e3d85d4a
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2022-10-19 22:00:21 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -2,11 +2,11 @@
 
 int main(){
  /*
-12345
-1234
-123
-12
-1
+12345                                               5     -> b (start or initialise)
+1234                                                4     -> b
+123        ---> Explanation --> for(int a = 1; a <= 3; a++) 
+12                                                  2     -> b
+1                                                   1     -> b (end)
 */
   
     for(int b = 5; b >= 1; b--){

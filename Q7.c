cf696bc044ba1632e0eb58b3b835cd969c8f954e
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2022-10-19 23:29:37 +0530

    Create Q7.c

---
+++ Q7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	
+/*
+1                                           1					     -> b (start or initialise)
+21                                          2     				 -> b
+321        ---> Explanation --> for(int a = 3; a >= 1; a--) 
+4321                                        4              -> b
+54321                                       5              -> b (end)
+*/
+    for(int b = 1; b <= 5; b++){
+        for(int a = b; a >= 1; a--){
+            printf("%d",a);
+        }
+        printf("\n");
+    }
+	
+	return 0;
+}

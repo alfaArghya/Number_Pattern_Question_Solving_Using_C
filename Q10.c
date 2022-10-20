25feca4226b2d45a6d0944415c5e28871145f689
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2022-10-20 21:44:01 +0530

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -2,11 +2,13 @@
 
 int main(){
   
-5
-44
-333
-2222
-11111
+/*
+5                                                   5     -> b
+44                                                  4     -> b
+333               explain --> for(int a = 1; a <= 6-3; a++)
+2222                                                2     -> b
+11111                                               1     -> b
+                  here we are printing b, that's why every line have same number.
 */
     for(int b = 5; b >= 1; b--){
         for(int a = 1; a <= 6-b; a++){

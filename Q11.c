12f5649e5217ec510f9eaf225ca9dc419a74f976
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2022-10-20 21:27:53 +0530

    Create Q11.c

---
+++ Q11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+	
+/*
+55555																						5			-> b
+4444																						4			-> b
+333							explain --> for(int a = 1; a <= 3; a++)
+22																							2			-> b
+1																								1			-> b
+								**here we are printing b that way every line is same.
+*/
+    for(int b = 5; b >= 1; b--){
+        for(int a = 1; a <= b; a++){
+            printf("%d",b);
+        }
+        printf("\n");
+    }
+	
+	return 0;
+}

7725fce92afa2af1d2b45a17cd5bc82dff95f3e6
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2022-10-21 10:51:34 +0530

    Update Q11.c

--- Q11.c
+++ Q11.c
@@ -3,11 +3,11 @@
 int main(){
 	
 /*
-55555						5     -> b
+55555						5     -> b(start or initialise)
 4444					        4     -> b
 333		explain --> for(int a = 1; a <= 3; a++)
 22				         	2     -> b
-1						1     -> b
+1						1     -> b (end)
 	**here we are printing b that way every line is same.
 */
     for(int b = 5; b >= 1; b--){

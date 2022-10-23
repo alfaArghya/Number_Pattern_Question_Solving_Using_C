bb24acc1c00a1ab77fe8e366ebc18aca9c9fe709
Arghya Das <95538110+alfaArghya@users.noreply.github.com>
2022-10-23 23:52:44 +0530

    Create Q46.c

---
+++ Q46.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int main(){
+  
+/*
+5 5 5 5 5 5 5 5 5
+5 4 4 4 4 4 4 4 5
+5 4 3 3 3 3 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 2 1 2 3 4 5
+5 4 3 2 2 2 3 4 5
+5 4 3 3 3 3 3 4 5
+5 4 4 4 4 4 4 4 5
+5 5 5 5 5 5 5 5 5
+*/
+
+    int c = 5; 
+    for(int b = c; b >= 1; b--){
+        for(int a = c; a >= 1; a--){
+            if (a>b){
+                printf("%d",a);
+            }
+            else{
+                printf("%d",b);
+            }
+        }
+        for(int a = 2; a <= c; a++){
+            if(a>b){
+                printf("%d",a);
+            }
+            else{
+                printf("%d",b);
+            }
+        }
+        printf("\n");
+    }
+
+    for(int b = 2; b <= c; b++){
+        for(int a = c; a >= 1; a--){
+            if (a>b){
+                printf("%d",a);
+            }
+            else{
+                printf("%d",b);
+            }
+        }
+        for(int a = 2; a <= c; a++){
+            if(a>b){
+                printf("%d",a);
+            }
+            else{
+                printf("%d",b);
+            }
+        }
+        printf("\n");
+    }
+
+
+  
+  return 0;
+}
